ecbdde01ddde151ceed1389aadfb652b9c41cf53
CIAN <3012839686.com@qq.com>
2026-05-12 19:41:28 +0800

    增加线下签到，堆排序实现签到循环队列排序

--- app.h
+++ app.h
@@ -47,6 +47,8 @@ typedef struct admission
 void init_on_line_queue();//初始化线上预约挂号数组
 void add_on_line(int number, char *name, char *admission, char *time);//添加线上预约挂号信息
 void sign_in_on_line(int number,int sub);//签到线上预约挂号
+void creat(int *ans, int root, int len);//建堆
+void heap_sort_sign_up_queue(Person *sign_up_queue, int size);//堆排序
 
 void menu_printf_all();//打印开始选择界面
 void menu_printf_on_line();//打印线上预约界面
@@ -56,6 +58,7 @@ int find_empty_on_line() ;//查看数组未编号位置
 void print_all_admissions() ;//打印就诊科室
 void init_admission_queue() ;//初始化就诊科室预约显示
 int find_admission(char *admission) ;//查看有没有对应科室
+
 
 // 全局变量声明（用extern）
 extern on_line on_line_queue[ON_LINE_SIZE];//线上预约挂号数组
--- on_line.c
+++ on_line.c
@@ -1,14 +1,16 @@
 #include "app.h"
 
-on_line on_line_queue[ON_LINE_SIZE];//线上预约挂号数组
+on_line on_line_queue[ON_LINE_SIZE]; // 线上预约挂号数组
 
-Person sign_up_queue[ON_LINE_SIZE];//签到排队队列
-int sign_up_front = 0;//签到排队队头
-int sign_up_rear = 0;//签到排队队尾
+Person sign_up_queue[ON_LINE_SIZE]; // 签到排队队列
+int sign_up_front = 0;              // 签到排队队头
+int sign_up_rear = 0;               // 签到排队队尾
 
-//初始化线上预约挂号数组
-void init_on_line_queue() {
-    for (int i = 0; i < ON_LINE_SIZE; i++) {
+// 初始化线上预约挂号数组
+void init_on_line_queue()
+{
+    for (int i = 0; i < ON_LINE_SIZE; i++)
+    {
         on_line_queue[i].number = 0;
         on_line_queue[i].name = NULL;
         on_line_queue[i].admission = NULL;
@@ -17,43 +19,93 @@ void init_on_line_queue() {
     }
 }
 
-//添加线上预约挂号信息
-void add_on_line(int number, char *name, char *admission, char *time) {
-    for (int i = 0; i < ON_LINE_SIZE; i++) {
-        if (on_line_queue[i].number == 0) { //找到第一个空位
+// 添加线上预约挂号信息
+void add_on_line(int number, char *name, char *admission, char *time)
+{
+    for (int i = 0; i < ON_LINE_SIZE; i++)
+    {
+        if (on_line_queue[i].number == 0)
+        { // 找到第一个空位
             on_line_queue[i].number = number;
             on_line_queue[i].name = strdup(name);
             on_line_queue[i].admission = strdup(admission);
             on_line_queue[i].time = strdup(time);
-            on_line_queue[i].tag = 0; //默认未签到
+            on_line_queue[i].tag = 0; // 默认未签到
             break;
         }
     }
 }
 
-//查看数组未编号位置
-int find_empty_on_line() {
-    for (int i = 0; i < ON_LINE_SIZE; i++) {
-        if (on_line_queue[i].number == 0) {
-            return i; //返回第一个空位的索引
+// 查看数组未编号位置
+int find_empty_on_line()
+{
+    for (int i = 0; i < ON_LINE_SIZE; i++)
+    {
+        if (on_line_queue[i].number == 0)
+        {
+            return i; // 返回第一个空位的索引
         }
     }
-    return -1; //没有空位
+    return -1; // 没有空位
 }
-//签到线上预约挂号
-void sign_in_on_line(int number,int sub) {
-    for (int i = 0; i < ON_LINE_SIZE; i++) {
-        if (on_line_queue[i].number == number) {
-            on_line_queue[i].tag = 1; //签到成功
-            //将签到的患者信息加入签到排队队列
+// 签到线上预约挂号
+void sign_in_on_line(int number, int sub)
+{
+    for (int i = 0; i < ON_LINE_SIZE; i++)
+    {
+        if (on_line_queue[i].number == number)
+        {
+            on_line_queue[i].tag = 1; // 签到成功
+            // 将签到的患者信息加入签到排队队列
             sign_up_queue[sign_up_rear].number = on_line_queue[i].number;
             sign_up_queue[sign_up_rear].name = strdup(on_line_queue[i].name);
             sign_up_queue[sign_up_rear].admission = strdup(on_line_queue[i].admission);
-            sign_up_queue[sign_up_rear].sub = sub; //设置是否需要复诊
-            sign_up_queue[sign_up_rear].if_sub = 0; //默认不是复诊排队
+            sign_up_queue[sign_up_rear].sub = sub;  // 设置是否需要复诊
+            sign_up_queue[sign_up_rear].if_sub = 0; // 默认不是复诊排队
             sign_up_queue[sign_up_rear].next = NULL;
-            sign_up_rear = (sign_up_rear + 1) % ON_LINE_SIZE; //循环队列
+            sign_up_rear = (sign_up_rear + 1) % ON_LINE_SIZE; // 循环队列
             break;
         }
+    }
+    heap_sort_sign_up_queue(sign_up_queue, (sign_up_rear - sign_up_front + ON_LINE_SIZE) % ON_LINE_SIZE);//每次签到都重新排序
+}
+
+//建堆
+void creat(int *ans, int root, int len)
+{
+    int child;
+    int temp = ans[root];
+    child = root * 2;
+    while (child <= len)
+    {
+        if (child < len && ans[child] < ans[child + 1])
+        {
+            child++;
+        }
+        if (temp > ans[child])
+        {
+            break;
+        }
+        ans[child / 2] = ans[child];
+        child *= 2;
+    }
+    ans[child / 2] = temp;
+}
+
+// 堆排序将签到队列按照大小顺序排序
+void heap_sort_sign_up_queue(Person *sign_up_queue, int size)
+{
+    // 构建最大堆
+    for (int i = size / 2; i > 0; i--)
+    {
+        creat((int *)sign_up_queue, i, size);
+    }
+    for (int i = size; i > 1; i--)
+    {
+        // 交换位置
+        Person temp = sign_up_queue[1];
+        sign_up_queue[1] = sign_up_queue[i];
+        sign_up_queue[i] = temp;
+        creat((int *)sign_up_queue, 1, i - 1);
     }
 }
