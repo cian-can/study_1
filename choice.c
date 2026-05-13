e1a7bcf719046767a982879596e93d84faee3cb0
CIAN <3012839686.com@qq.com>
2026-05-13 21:37:18 +0800

    优化初始化位置

--- choice.c
+++ choice.c
@@ -4,9 +4,18 @@
 //选择界面
 void choice(){
     int choice;
+
+    init_admission_queue();
+    init_on_line_queue();
+    init_off_line_queue();
+    init_sign_up_queue();
+    init_all_off_line_queue() ;
+
+    // test_add_on_line();
+    // test_sign_in_on_line();
     while (1) {
         menu_printf_all();
-        init_admission_queue();
+        merge_queues();//每次进入选择界面都将线下排队和签到队列合并
         printf("请输入您的选择：");
         scanf("%d", &choice);
         switch (choice) {
@@ -24,6 +33,7 @@ void choice(){
             case 4:
                 //显示总体排队情况
                 printf_overall_queue();
+                print_sign_up_queue();
                 break;
             case 5:
                 //叫号
