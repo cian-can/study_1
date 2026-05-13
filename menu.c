3f49175a59dc5f4acc575174a857c9a4c90ee466
CIAN <3012839686.com@qq.com>
2026-05-13 14:28:02 +0800

    优化了科室不匹配导致直接退出的问题，增加了线下挂号功能

--- menu.c
+++ menu.c
@@ -5,7 +5,9 @@ void menu_printf_all(){
     printf("1. 线上预约挂号\n");
     printf("2. 线下排队挂号\n");
     printf("3. 签到\n");
-    printf("4. 退出系统\n");
+    printf("4. 显示总体排队情况\n");
+    printf("5. 叫号\n");
+    printf("6. 退出系统\n");
 }
 
 //打印线上预约界面
@@ -21,13 +23,20 @@ void menu_printf_on_line(){
     {
         print_all_admissions();
     }
-    printf("请输入就诊科室：");
     char admission[50];
-    scanf("%s", admission);
-    if(find_admission(admission) == -1)
+    while(1)
     {
-        printf("没有找到对应科室，请重新输入！\n");
-        return;
+        printf("请输入就诊科室：");
+        scanf("%s", admission);
+        if(find_admission(admission) == -1)
+        {
+            printf("没有找到对应科室，请重新输入！\n");
+            continue;
+        }
+        else
+        {
+            break;
+        }
     }
     time_t now = time(NULL);
     struct tm *tm_now = localtime(&now);
@@ -68,13 +77,48 @@ else if(number >10 ){
     printf("预约成功！您的取号顺序是：%d\n", number + 1);
 }
 
+//线下排队界面
+void menu_printf_off_line(){
+    printf("线下排队挂号\n");
+    printf("请输入患者姓名：");
+    char name[50];
+    scanf("%s", name);
+    printf("需要查看科室信息请输入1，直接输入科室名字请输入2：");
+    int choice;
+    scanf("%d", &choice);
+    if(choice == 1)
+    {
+        print_all_admissions();
+    }
+    char admission[50];
+    while(1)
+    {
+        printf("请输入就诊科室：");
+        scanf("%s", admission);
+        if(find_admission(admission) == -1)
+        {
+            printf("没有找到对应科室，请重新输入！\n");
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    }
+    int sub;
+    printf("需要复诊请输入1，不需要复诊请输入0：");
+    scanf("%d", &sub);
+    add_off_line(off_line_rear + 1, name, admission, sub);
+    printf("挂号成功！您的取号顺序是：%d\n", off_line_rear);
+}
+
 //签到选项
 void menu_printf_sign_in(){
     printf("签到界面\n");
     printf("请输入您的取号顺序：");
     int number;
     scanf("%d", &number);
-    // 添加功能：判断输入的取号顺序是否存在，如果不存在则提示用户重新输入
+    //判断输入的取号顺序是否存在，如果不存在则提示用户重新输入
         int found = 0;
     for (int i = 0; i < ON_LINE_SIZE; i++) {
         if (on_line_queue[i].number == number) {
