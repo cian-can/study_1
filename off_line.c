7433a383899de076402dfff69827aa2295efe163
CIAN <3012839686.com@qq.com>
2026-05-13 14:29:59 +0800

    实现了线下排队挂号，线下排队队列合签到队列的合并

--- off_line.c
+++ off_line.c
@@ -5,6 +5,10 @@ Person off_line_queue[OFF_LINE_SIZE];//线下排队队列
 int off_line_front = 0;//线下排队队头
 int off_line_rear = 0;//线下排队队尾
 
+Person all_off_line_queue[OFF_LINE_SIZE];//线下排队和签到合并后的队列
+int all_off_line_front = 0;//合并后队头
+int all_off_line_rear = 0;//合并后队尾
+
 //初始化线下排队队列
 void init_off_line_queue() {
     for (int i = 0; i < OFF_LINE_SIZE; i++) {
@@ -27,4 +31,33 @@ void add_off_line(int number, char *name, char *admission, int sub) {
     off_line_queue[off_line_rear].if_sub = 0; //默认不是复诊排队
     off_line_queue[off_line_rear].next = NULL;
     off_line_rear = (off_line_rear + 1) % OFF_LINE_SIZE; //循环队列
+}
+
+//线下排队和签到队列合并
+void merge_queues() {
+
+    while(1)
+    {
+        if(off_line_front != off_line_rear) //线下排队队列不为空
+        {
+            all_off_line_queue[all_off_line_rear] = off_line_queue[off_line_front];
+            all_off_line_rear = (all_off_line_rear + 1) % OFF_LINE_SIZE;
+            off_line_front = (off_line_front + 1) % OFF_LINE_SIZE;
+        }
+        else if(sign_up_front != sign_up_rear) //签到队列不为空
+        {
+            all_off_line_queue[all_off_line_rear].number = sign_up_queue[sign_up_front].number;
+            all_off_line_queue[all_off_line_rear].name = strdup(sign_up_queue[sign_up_front].name);
+            all_off_line_queue[all_off_line_rear].admission = strdup(sign_up_queue[sign_up_front].admission);
+            all_off_line_queue[all_off_line_rear].sub = sign_up_queue[sign_up_front].sub;
+            all_off_line_queue[all_off_line_rear].if_sub = sign_up_queue[sign_up_front].if_sub;
+            all_off_line_queue[all_off_line_rear].next = NULL;
+            all_off_line_rear = (all_off_line_rear + 1) % OFF_LINE_SIZE;
+            sign_up_front = (sign_up_front + 1) % ON_LINE_SIZE;
+        }
+        else
+        {
+            break; //两个队列都为空，合并完成
+        }
+    }
 }
